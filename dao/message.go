7de336f10499c03aaa871e9c88a313b19198c2fc
husterdjx <76643896@qq.com>
2022-11-01 09:53:48 +0800

    fix:change is_new to int64

--- dao/message.go
+++ dao/message.go
@@ -6,8 +6,8 @@ func (rs *RdbService) CreateMessage(meg []*model.Message) error {
 	return rs.tx.Create(&meg).Error
 }
 
-func (rs *RdbService) SyncMessage(receiverId int64, syncSeq int64, limit int, isNew bool) (megs []model.Message, err error) {
-	if isNew {
+func (rs *RdbService) SyncMessage(receiverId int64, syncSeq int64, limit int, isNew int64) (megs []model.Message, err error) {
+	if isNew > 0 {
 		err = rs.tx.Table("messages").Where("receiver_id = ? AND Seq <= ?", receiverId, syncSeq).Order("seq DESC").Limit(limit).Find(&megs).Error
 	} else {
 		err = rs.tx.Table("messages").Where("receiver_id = ? AND Seq >= ?", receiverId, syncSeq).Order("seq ASC").Limit(limit).Find(&megs).Error
