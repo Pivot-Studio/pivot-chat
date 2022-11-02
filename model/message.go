e280b3d46e5594dcc9fc0e61b3401ca50a3bbc89
husterdjx <76643896@qq.com>
2022-11-02 17:39:12 +0800

    feat:add encrypt

--- model/message.go
+++ model/message.go
@@ -6,7 +6,7 @@ type Message struct {
 	MessageId  int64     `gorm:"primarykey"` // 自增主键
 	SenderId   int64     // 发送者账户id
 	ReceiverId int64     `gorm:"unique_index:u_meg"` // 接收者id,均为group_id
-	Content    string    // 消息内容
+	Content    []byte    // 消息内容
 	Seq        int64     `gorm:"unique_index:u_meg"` // 消息同步序列
 	SendTime   time.Time // 消息发送时间（落库时间）
 	Type       int64
