3f764e9dd032d4a520c86fdb1e7a31a7981b4f26
husterdjx <76643896@qq.com>
2022-11-03 16:01:44 +0800

    test

--- dao/message.go
+++ dao/message.go
@@ -1,12 +1,17 @@
 package dao
 
-import "github.com/Pivot-Studio/pivot-chat/model"
+import (
+	"fmt"
+
+	"github.com/Pivot-Studio/pivot-chat/model"
+)
 
 func (rs *RdbService) CreateMessage(meg []*model.Message) error {
 	return rs.tx.Create(&meg).Error
 }
 
 func (rs *RdbService) SyncMessage(receiverId int64, syncSeq int64, limit int, isNew int64) (megs []model.Message, err error) {
+	fmt.Println("ISNEW: ", isNew)
 	if isNew > 0 {
 		err = rs.tx.Table("messages").Where("receiver_id = ? AND Seq >= ?", receiverId, syncSeq).Order("seq DESC").Limit(limit).Order("seq").Find(&megs).Error
 	} else {
