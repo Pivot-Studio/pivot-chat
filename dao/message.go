6d99b971d3ad33f293b71b3550df4003e2e60806
husterdjx <76643896@qq.com>
2022-11-03 16:29:06 +0800

    test

--- dao/message.go
+++ dao/message.go
@@ -13,7 +13,8 @@ func (rs *RdbService) CreateMessage(meg []*model.Message) error {
 func (rs *RdbService) SyncMessage(receiverId int64, syncSeq int64, limit int, isNew int64) (megs []model.Message, err error) {
 	fmt.Println("ISNEW: ", isNew)
 	if isNew > 0 {
-		err = rs.tx.Debug().Table("messages").Where("receiver_id = ? AND Seq >= ?", receiverId, syncSeq).Order("seq desc").Limit(limit).Order("seq").Find(&megs).Error
+		tmp := rs.tx.Debug().Table("messages").Where("receiver_id = ? AND Seq >= ?", receiverId, syncSeq).Order("seq desc").Limit(limit)
+		err = tmp.Order("seq").Find(&megs).Error
 	} else {
 		err = rs.tx.Table("messages").Where("receiver_id = ? AND Seq >= ?", receiverId, syncSeq).Order("seq").Limit(limit).Find(&megs).Error
 	}
