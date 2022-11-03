adc7cf4f2e39436338af52ab39b0d95d16433e5f
husterdjx <76643896@qq.com>
2022-11-03 15:56:11 +0800

    fix:change sync logic

--- dao/message.go
+++ dao/message.go
@@ -8,7 +8,7 @@ func (rs *RdbService) CreateMessage(meg []*model.Message) error {
 
 func (rs *RdbService) SyncMessage(receiverId int64, syncSeq int64, limit int, isNew int64) (megs []model.Message, err error) {
 	if isNew > 0 {
-		err = rs.tx.Table("messages").Where("receiver_id = ? AND Seq >= ?", receiverId, syncSeq).Order("seq DESC").Limit(limit).Find(&megs).Error
+		err = rs.tx.Table("messages").Where("receiver_id = ? AND Seq >= ?", receiverId, syncSeq).Order("seq DESC").Limit(limit).Order("seq ASC").Find(&megs).Error
 	} else {
 		err = rs.tx.Table("messages").Where("receiver_id = ? AND Seq >= ?", receiverId, syncSeq).Order("seq ASC").Limit(limit).Find(&megs).Error
 	}
